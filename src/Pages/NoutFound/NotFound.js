d235c822584245828bf2dab607e3616e50c3d326
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-07 13:45:08 -0300

    Atulizando pagina NotFound com tema claro e escuro, e adicionando o botão Back voltando para a pagina anterior

--- src/Pages/NoutFound/NotFound.js
+++ src/Pages/NoutFound/NotFound.js
@@ -3,13 +3,17 @@ import {VscDebugDisconnect, VscSignIn, VscBug} from 'react-icons/vsc'
 import Logo from './../../Icons/FavIcon.png'
 
 function NotFoundPage() {
+
+    function Back(){
+        window.history.back()
+    }
     return (
         <div className='NotFoundPage'>
             <div>
                 <p><VscBug/> Oops..</p>
                 <h1><VscDebugDisconnect/> 404 </h1>
                 <p> página não encontrada! </p>
-                <a href='/'><VscSignIn/>Voltar</a>
+                <button onClick={()=> Back()}><VscSignIn/>Voltar</button>
             </div>
             <div>
                 <img  alt='logo' src={Logo}/>
