7dda117d11da1ba00c19db765703cb6ec32b43a3
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-09 18:58:59 -0300

    Atualizando algumas functions de desenvolvimento

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,15 +3,36 @@ import Navbar from './../../Components/NavBar/NavBar'
 
 
 function HomePage() {
-    function Teste(){
+    function ConsoleLogStorage(){
         console.log(window.sessionStorage)
         console.log(window.localStorage)
+    }
+    function CreateSession(){
+        window.sessionStorage.setItem('session', 'on')
+    }
+    function CreateLocalStorage(){
+        window.localStorage.setItem('Mode', 'light')
+        window.localStorage.setItem('login', 'TesteEmail@gmail.com')
+        window.localStorage.setItem('imgProfile', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fprofile%2F&psig=AOvVaw0re8eQzJj1RYsVfLs3hF3s&ust=1699646603601000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCODd38nat4IDFQAAAAAdAAAAABAE')
+        window.localStorage.setItem('name', 'Test Acount')
+        window.localStorage.setItem('passwordCrypt', 'Passcrypt')
+    }
+    function ResetSession(){
+        window.sessionStorage.clear()
+    }
+    function ResetLocalStorage(){
+        window.localStorage.clear()
     }
     return (
         <>
             <Navbar/>
             <h1>Home</h1>
-            <button onClick={()=> Teste()}>Funcito Test</button>
+            <button onClick={()=> ConsoleLogStorage()}>ConsoleLogStorage Test</button>
+            <button onClick={()=> CreateSession()}>Iniciar Session Test</button>
+            <button onClick={()=> CreateLocalStorage()}>Iniciar localStorage Test</button>
+        
+            <button onClick={()=> ResetSession()}>Reset sessionStorage</button>
+            <button onClick={()=> ResetLocalStorage()}>Reset localStorage</button>
             <fieldset style={{width: '200px'}}>
                 <legend>Pages:</legend>
                 <ul>
