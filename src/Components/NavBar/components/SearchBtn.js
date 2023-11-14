32379888505117acbaa080ac7f96e256052bd5c3
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-14 14:32:50 -0300

    Atualizando SearchButton na Navbar

--- src/Components/NavBar/components/SearchBtn.js
+++ src/Components/NavBar/components/SearchBtn.js
@@ -12,6 +12,22 @@ function SearchNavBtn(){
             document.getElementById('BtnClearSearch').style.opacity = '1'
         }
     }
+
+    function SearchMovie(){
+        var LoadElement = document.getElementById('BtnLoader')
+        var SearchElement = document.getElementById('BtnSearch')
+        LoadElement.style.display = 'flex'
+        SearchElement.style.display = 'none'
+        if(SearchMovieName === '' || SearchMovieName === undefined){
+            setTimeout(function(){
+                LoadElement.style.display = 'none'
+                SearchElement.style.display = 'flex'
+            },1000)
+            return
+        }
+        window.location = `/search?m=${SearchMovieName}`
+    }
+
     return (
         <div className='MenuBar'>
             <div className='SearchDiv'>
@@ -21,9 +37,15 @@ function SearchNavBtn(){
                     onChange={(e)=> UpdateValueMovieName(e.target.value)} 
                     className='InputSearch' 
                     type='text' 
-                    placeholder='Search Movie...' />
+                    placeholder='Search Movie...'
+                    onKeyDown={event => {
+                        if (event.key === 'Enter') {
+                            SearchMovie()
+                        }
+                        }} 
+                        />
                 <button onClick={()=> UpdateValueMovieName('')} id='BtnClearSearch' className='BtnClearSearch'><BiEraser size={20}/></button>
-                <div className='BtnSearch'><button id='BtnSearchOff'><BiSearchAlt2 size={30}/></button> <button id='BtnSearchON'><BiSearchAlt2 size={30}/></button></div>
+                <div className='BtnSearch'><button onClick={()=> SearchMovie()} id='BtnSearch'><BiSearchAlt2 size={30}/></button><span id='BtnLoader' class="loader"></span></div>
             </div>
         </div>
     )
