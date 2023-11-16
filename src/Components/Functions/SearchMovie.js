1b0d05cbb2be01f2a859e7bef01e117a05c437f1
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-16 16:21:00 -0300

    Atualizando function SearchMovie

--- src/Components/Functions/SearchMovie.js
+++ src/Components/Functions/SearchMovie.js
@@ -1,26 +1,96 @@
 import axios from "axios"
 
+
 export async function SearchMovie(value){
 
-    const options = {
-        method: 'GET',
-        url: 'https://streaming-availability.p.rapidapi.com/search/title',
-        params: {
-          title: value,
-          country: 'us',
-          show_type: 'all',
-          output_language: 'en'
-        },
-        headers: {
-          'X-RapidAPI-Key': '4d1fc03470msh98ed2d469a33f37p102184jsn7cab8e913b66',
-          'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+      var MoviesObj = []
+      var MoviesLenght = await SearchMovieLength(value).then(function(result) {
+        return result
+      });
+      const api = axios.create({
+        baseURL: 'https://www.omdbapi.com/'
+      })
+
+      console.log('Loading...')
+      var i = 0
+      while(i < MoviesLenght){
+        const response = await api.get(`?i=${await SearchMovieId(value, i)}&apikey=c74f3650`);
+       
+        var Runtimevalue = 'Not found Runtime'
+        if(response.data.Runtime === 'N/A'){
+          Runtimevalue = 'Not found Runtime'
+        }else{
+          Runtimevalue = response.data.Runtime
         }
-      };
-      
-      try {
-          const response = await axios.request(options);
-          console.log(response.data);
-      } catch (error) {
-          console.error(error);
+        MoviesObj.push(
+            {name: response.data.Title, 
+            banner: response.data.Poster, 
+            description: response.data.Plot, 
+            trailer: response.data.trailer, 
+            year: response.data.Year,
+            geners: response.data.Genre,
+            Released: response.data.Released,
+            Runtime: Runtimevalue,
+            }
+        )
+        if(i === 20){
+          break
+        }
+
+        i++
       }
-}
+      console.log('Loading completed!')
+      
+      return (MoviesObj)
+
+      
+    
+  }
+
+  async function SearchMovieId(title, number){
+    const optionsSearchID = {
+      method: 'GET',
+      url: 'https://moviesdatabase.p.rapidapi.com/titles/search/title/' + title,
+      params: {
+        titleType: 'movie',
+        limit: '20'
+      },
+      headers: {
+        'X-RapidAPI-Key': '4d1fc03470msh98ed2d469a33f37p102184jsn7cab8e913b66',
+        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+      }
+    };
+    try {
+      const responseID = await axios.request(optionsSearchID);
+      return responseID.data.results[number].id
+      // const responseSearchDetails = await axios.request(optionsSearchDetails);
+      //return responseID.data
+      
+    } catch (error) {
+      return (error)
+    }
+  }
+  async function SearchMovieLength(title){
+    const optionsSearchLength= {
+      method: 'GET',
+      url: 'https://moviesdatabase.p.rapidapi.com/titles/search/title/' + title,
+      params: {
+        titleType: 'movie',
+        limit: '20'
+      },
+      headers: {
+        'X-RapidAPI-Key': '4d1fc03470msh98ed2d469a33f37p102184jsn7cab8e913b66',
+        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+      }
+    };
+    try {
+      const responseID = await axios.request(optionsSearchLength);
+      return responseID.data.results.length
+      // const responseSearchDetails = await axios.request(optionsSearchDetails);
+      //return responseID.data
+      
+    } catch (error) {
+      return (error)
+    }
+  }
+
--- src/Pages/SearchMovie/SearchMovie.js
+++ src/Pages/SearchMovie/SearchMovie.js
@@ -2,17 +2,60 @@ import './SearchMovie.css';
 import Navbar from './../../Components/NavBar/NavBar'
 
 import { setMovieSearch } from '../../Components/NavBar/components/SearchBtn';
+import { SearchMovie } from '../../Components/Functions/SearchMovie';
 
 
 function SearchMoviePage() {
     const urlParams = new URLSearchParams(window.location.search);
     const MovieName = urlParams.get("m")
+    var MovieObj = {}
     
-    function SearchMovie(){
-        console.log(MovieName)
+    function StartSearchMovie(){
         setMovieSearch(MovieName)
+
+
+        SearchMovie(MovieName).then(function(result) {
+            // console.log(result)
+            var i = 0
+            var MoviesObjSearch  = []
+            while(i < result.length){
+                MoviesObjSearch.push(
+                    {name: result[i].name,
+                    banner: result[i].banner,
+                    description: result[i].description,
+                    trailer: result[i].trailer,
+                    year: result[i].banner,
+                    geners: result[i].geners,
+                    Released: result[i].Released,
+                    Runtime: result[i].Runtime,
+                    
+                    }
+                )
+                i++
+            }
+
+            /* 
+                name: response.data.Title, 
+                banner: response.data.Poster, 
+                description: response.data.Plot, 
+                trailer: response.data.trailer, 
+                year: response.data.Year,
+                geners: response.data.Genre,
+                Released: response.data.Released,
+                Runtime: Runtimevalue,
+            */
+            console.log(result)
+            console.log(MoviesObjSearch)
+            MovieObj = MoviesObjSearch
+        });
+
     }
-    setTimeout(SearchMovie,10)
+    setTimeout(StartSearchMovie,10)
+
+    
+    function teste(){
+        console.log(MovieObj)
+    }
 
 
 
@@ -20,6 +63,7 @@ function SearchMoviePage() {
         <>
             <Navbar/>
             <h1>SearchMovie</h1>
+            <button onClick={()=> teste()}> TESTEEEEE</button>
         </>
     );
   }
