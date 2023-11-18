c73c12286047b69d057dc68c4b14f542b806b5f5
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-18 16:34:11 -0300

    Adicionando Função de mudar de pagina ao site

--- src/Pages/SearchMovie/SearchMovie.js
+++ src/Pages/SearchMovie/SearchMovie.js
@@ -1,8 +1,7 @@
 import './SearchMovie.css';
 import Navbar from './../../Components/NavBar/NavBar'
 
-import { BiSearchAlt } from "react-icons/bi";
-
+import { BiSearchAlt,BiSolidChevronLeft ,BiSolidChevronRight   } from "react-icons/bi";
 import { setMovieSearch } from '../../Components/NavBar/components/SearchBtn';
 
 import  api  from '../../Components/Functions/SearchMovie';
@@ -14,10 +13,11 @@ import { useState } from 'react';
 var MoviesPages = 0
 function SearchMoviePage() {
     const [Topics , SetTopics] = useState([])
+    const [PagesControl, setPagesControl] = useState([])
     const urlParams = new URLSearchParams(window.location.search);
     const MovieName = urlParams.get("m")
     var MovieSearchPage = urlParams.get("page")
-    if(MovieSearchPage === undefined || isNaN(MovieSearchPage)){
+    if(MovieSearchPage === undefined || isNaN(MovieSearchPage) || MovieSearchPage === '' || MovieSearchPage === null){
         MovieSearchPage = 1
     }
     
@@ -35,30 +35,102 @@ function SearchMoviePage() {
 
         const response = await api.get(`?s=${MovieName}&apikey=c74f3650&page=${MovieSearchPage}`);
         SetTopics(response.data.Search)
-      
-
+        PagesCount()    
 
     }
     setTimeout(StartSearchMovie,10)
     
 
     function TopicsBox({item}){
-        console.log(item)
+
         return <div className="MovieCard">
                     <img  alt={' image: '+  item.Title} src={item.Poster}/>
                     <span>{item.Title}</span>
                 </div>
     }
 
+    function PagesCount(){
+        var ControlObj = []
+        var FirstPage = 1
+        var PageAtual = parseInt(MovieSearchPage)
+        var PageAnterior = parseInt(MovieSearchPage) -1
+        var PageSeguinte = parseInt(MovieSearchPage) + 1
+        var LastPage = parseInt(MoviesPages)
+        ControlObj.push(
+            {FirstPage: FirstPage}
+        )
+
+        if(PageAnterior < FirstPage){ // LeftControl validation!
+            // Não adiciona Seta para a esquerda! LeftControl = false
+        }else{
+
+            // Adiciona Seta para a esquerda! LeftControl = true
+            ControlObj.push(
+                {LeftControl: '<', proxPage: PageAnterior}
+            )
+        }
+    
+        if(PageAtual === FirstPage){
+            //console.log('Pagina atual: | ' + PageAtual +' |')
+        }else{
+            ControlObj.push(
+                {PageNumber: PageAnterior }, {PageNumber: PageAtual}
+            )
+        }
+        
+        if(PageSeguinte > LastPage){
+
+        }else{
+
+            ControlObj.push(
+                {PageNumber: PageSeguinte}, {RightControl: '>', proxPage: PageSeguinte}, {LastPage: LastPage}
+            )
+        }
+        setPagesControl(ControlObj)
+
+    }
+
+    function PagesControlBox({item}){
+        var value = 't'
+        var FunctionValue = 0
+        if(item.FirstPage !== undefined){
+            value = item.FirstPage
+            FunctionValue = item.FirstPage
+        }
+        if(item.LeftControl !== undefined){
+            value = <BiSolidChevronLeft />
+            FunctionValue =  item.proxPage
+        }
+        if(item.PageNumber){
+            value =  item.PageNumber
+            FunctionValue =  item.PageNumber
+        }
+        if(item.LastPage !== undefined){
+            value = item.LastPage
+            FunctionValue =  item.LastPage
+        }
+        if(item.RightControl !== undefined){
+            value = <BiSolidChevronRight/>
+            FunctionValue =  item.proxPage
+        }
+        return <button onClick={()=> NewPage(FunctionValue)}>{value}</button>
+    }
+    function NewPage(value){
+        window.location.search = '?m=' + MovieName + '&page=' + value
+    }
     return (
         <>
             <Navbar/>
             <main className='SearchPage'>
                 <div className='TitlesMovies'>
-                    <h1 id='MovieNameId'>Movie name</h1> <BiSearchAlt  size={30}/>
+                    <h1 id='MovieNameId'></h1> <BiSearchAlt  size={30}/>
                 </div>
                 <div id='list-movies' className='ListMovies'>
                     {Topics.map((Topics, index) => <TopicsBox item={Topics} />)}
+                </div>
+                <div className='MovieListOtions'>
+                    <div>{PagesControl.map((PagesControl, index) => <PagesControlBox item={PagesControl} />)}</div>
+                    <span>{MovieSearchPage}/{MoviesPages}</span>
                 </div>
             </main>
         </>
