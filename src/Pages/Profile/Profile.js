23909ef73ac0d2483421948ad218c71ad7e05cac
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-24 18:35:11 -0300

    Adicionando um aviso em /profile

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,13 +1,20 @@
+
+// import css
 import './Profile.css';
+
+// import components
 import Navbar from './../../Components/NavBar/NavBar'
+
+// import icons
+import { BiPackage } from 'react-icons/bi';
 
 
 function ProfilePage() {
     return (
-        <>
+        <main className='profilePage'>
             <Navbar/>
-            <h1>Profile</h1>
-        </>
+            <h2><BiPackage /> Profile is still being created, come back later, click <a href={false} onClick={()=> window.history.back()}>HERE</a> to return </h2>
+        </main>
     );
   }
   
