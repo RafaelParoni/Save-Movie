7eba2300a2d0996765d4ab8756ddbddef226a648
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-24 13:57:49 -0300

    Adicionando Functions ( TestSaveMovie, SaveMovie)
    TestSaveMovie - verifica se já está existente no banco de dados aquele filme selecionado.
    
    SaveMovie - Salva o filme ou Deleta o filme do banco de dados

---
+++ src/Components/Functions/SaveMovie.js
@@ -0,0 +1,45 @@
+// Banco de dados
+import { initializeApp   } from "firebase/app";
+import {collection, getDocs, getFirestore, setDoc, doc, deleteDoc} from "firebase/firestore";
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyCi1wYTsZBEVZp2zAQquOY8mYp7ZTe3Mnw",
+    authDomain: "savemovie-e7ea6.firebaseapp.com",
+    projectId: "savemovie-e7ea6",
+    storageBucket: "savemovie-e7ea6.appspot.com",
+    messagingSenderId: "613203193395",
+    appId: "1:613203193395:web:8430b6069114d133157d46",
+    measurementId: "G-GV5GW3T3ZV"
+});
+
+
+
+
+export async function SaveMovie(NameMovie, IdMovie, PosterMovie){
+
+
+    var MovieSave = {
+        name: NameMovie,
+        id: IdMovie,
+        poster: PosterMovie,
+    }
+
+    const db = getFirestore(firebaseApp);
+    var collectionUser = 'saves-' + window.localStorage.getItem('id')
+    const SavesUserID = collection(db, collectionUser);
+    const userDoc = doc(db, collectionUser, IdMovie)
+
+    const data = await getDocs(SavesUserID);
+    var SavesUser = data.docs.map((doc) => ({...doc.data(), id: doc.id}))
+    var i = 0
+     while(i < SavesUser.length){
+         if(SavesUser[i].id === IdMovie ){ // Verifica se já tem um Filme salvo se se for true deleta o filme do bd
+            if(!window.confirm('Deseja Deletar ' + NameMovie + 'de sua conta?')){return}
+            await deleteDoc(userDoc);
+            return
+         }
+         i++
+     }
+     if(!window.confirm('Deseja salvar ' + NameMovie + ' em sua conta?')){return}
+     await setDoc(doc(db, collectionUser, IdMovie), MovieSave); // Salva um filme no bd
+    }
---
+++ src/Components/Functions/TestSaveMovie.js
@@ -0,0 +1,34 @@
+// Banco de dados
+import { initializeApp   } from "firebase/app";
+import {collection, getDocs, getFirestore} from "firebase/firestore";
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyCi1wYTsZBEVZp2zAQquOY8mYp7ZTe3Mnw",
+    authDomain: "savemovie-e7ea6.firebaseapp.com",
+    projectId: "savemovie-e7ea6",
+    storageBucket: "savemovie-e7ea6.appspot.com",
+    messagingSenderId: "613203193395",
+    appId: "1:613203193395:web:8430b6069114d133157d46",
+    measurementId: "G-GV5GW3T3ZV"
+});
+
+
+
+
+export async function TestSaveMovie(IdMovie){
+
+    const db = getFirestore(firebaseApp);
+    var collectionUser = 'saves-' + window.localStorage.getItem('id')
+    const SavesUserID = collection(db, collectionUser);
+
+    const data = await getDocs(SavesUserID);
+    var SavesUser = data.docs.map((doc) => ({...doc.data(), id: doc.id}))
+    var i = 0
+     while(i < SavesUser.length){
+         if(SavesUser[i].id === IdMovie ){ // Verifica se já tem um Filme salvo se se for true deleta o filme do bd
+            return (true)
+         }
+         i++
+     }
+     return (false)
+    }
