1358695ad75d184d1acba9a274431577199e5d4c
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-26 14:27:49 -0300

    Arrumando bug na function de buscar somente o mesmo filme e não o filme escolhido pelo o usuário

--- src/Components/Functions/StremingSearch.js
+++ src/Components/Functions/StremingSearch.js
@@ -1,13 +1,13 @@
  import axios from "axios";
  
  
- export async function StremingSearch(){
+ export async function StremingSearch(id){
     const options = {
         method: 'GET',
         url: 'https://streaming-availability.p.rapidapi.com/get',
         params: {
           output_language: 'en',
-          imdb_id: 'tt4154664'
+          imdb_id: id
         },
         headers: {
           'X-RapidAPI-Key': '4d1fc03470msh98ed2d469a33f37p102184jsn7cab8e913b66',
