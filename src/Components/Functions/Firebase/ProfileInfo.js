96878e03c3f6de44568d4df30a50a517935242eb
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-27 18:48:13 -0300

    Adicionando function de buscar informações  do usuário

---
+++ src/Components/Functions/Firebase/ProfileInfo.js
@@ -0,0 +1,34 @@
+// Banco de dados
+import { initializeApp   } from "firebase/app";
+import {collection, getDocs, getFirestore, setDoc, doc} from "firebase/firestore";
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyCi1wYTsZBEVZp2zAQquOY8mYp7ZTe3Mnw",
+    authDomain: "savemovie-e7ea6.firebaseapp.com",
+    projectId: "savemovie-e7ea6",
+    storageBucket: "savemovie-e7ea6.appspot.com",
+    messagingSenderId: "613203193395",
+    appId: "1:613203193395:web:8430b6069114d133157d46",
+    measurementId: "G-GV5GW3T3ZV"
+});
+
+
+export async function ProfileInfo(iduser){
+    if(window.sessionStorage.getItem('session') === 'on'){
+        const db = getFirestore(firebaseApp);
+        var collectionUser = 'users'
+        const userCollection = collection(db, collectionUser);
+
+        const data = await getDocs(userCollection);
+        var InfoUser = data.docs.map((doc) => ({...doc.data(), id: doc.id}))
+        var i = 0
+        while(i < data.docs.map((doc) => ({...doc.data(), id: doc.id})).length){
+            if(iduser === data.docs.map((doc) => ({...doc.data(), id: doc.id}))[i].id){
+                InfoUser = data.docs.map((doc) => ({...doc.data(), id: doc.id}))[i]
+            }
+            i++
+        }
+        
+        return InfoUser
+    }
+} 
