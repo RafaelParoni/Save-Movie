bd67d52f9c748c6d04cc2720ca5e74d4b46c2c5f
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-28 18:08:06 -0300

    Adicioando .env em. gitigone e adicionando importKeys.js a código

---
+++ src/Components/Functions/keys/importKey.js
@@ -0,0 +1,9 @@
+export const OMDB_API_URL = process.env.REACT_APP_OMDB_API_URL // SearchMovie.js
+
+export const MOVIEDATABASE_KEY = process.env.REACT_APP_MOVIEDATABASE_API_KEY // DetailsMovie.js - RandomMovies.js
+
+export const STREAMING_KEY = process.env.REACT_APP_STREAMING_API_KEY // StremingSearch.js
+
+export const MOVIE_MINI_DATABASE_KEY = process.env.REACT_APP_MOVIES_MINI_DATABASE_API_KEY // TrailerMovieSearch.js
+
+export const FIREBASE_KEY = process.env.REACT_APP_FIREBASE_API_KEY
