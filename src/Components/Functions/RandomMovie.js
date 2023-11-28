1ab9d7e70d2a340ff1f0120e665a67c2cf4ac4b1
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-11-28 18:12:28 -0300

    Alterando key de RandomMovies para variável  ambiente

--- src/Components/Functions/RandomMovie.js
+++ src/Components/Functions/RandomMovie.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { MOVIEDATABASE_KEY } from "./keys/importKey";
 export async function RandomMoviesApi(){
     const options = {
       method: 'GET',
@@ -8,7 +8,7 @@ export async function RandomMoviesApi(){
         list: 'top_rated_series_250'
       },
       headers: {
-        'X-RapidAPI-Key': '4d1fc03470msh98ed2d469a33f37p102184jsn7cab8e913b66',
+        'X-RapidAPI-Key': MOVIEDATABASE_KEY,
         'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
       }
     };
